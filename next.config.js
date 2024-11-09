e4c57f97fc14a80299c125d9ac895b07e7c85585
Allam2021-aqar <aqardrug@gmail.com>
2024-11-09 12:54:50 +0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['res.cloudinary.com','asset.cloudinary.com','img.freepik.com','firebasestorage.googleapis.com','upload.wikimedia.org'],
+      },
+}
+
+module.exports = nextConfig
